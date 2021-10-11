66d84fe67604ae4e05a79ead8091747c6542486b
bvegaM <vegabryam40@gmail.com>
2021-10-11 14:02:17 -0500

    add readme.md

--- length_of_longest_substring.py
+++ length_of_longest_substring.py
@@ -33,13 +33,12 @@ class Solution:
                     pop = dque_list.popleft()  
             dque_list.append(char)
             max_len = max(max_len,len(dque_list))
-        
         return max_len
 
 if __name__ == '__main__':
     s = 'pwwkew'
     solution  = Solution()
     reponse = solution.lengthOfLongestSubstring(s)
-
+    print(reponse)
 
     
---
+++ readme.md
@@ -0,0 +1,6 @@
+# LeetCode Problems
+
+![leetcode](https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/LeetCode_Logo_black_with_text.svg/1280px-LeetCode_Logo_black_with_text.svg.png)
+
+[![Contributors][contributors-shield]][contributors-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
